510214769dacc4d98d9dad7ce20ca3ae86e6ec17
jchozas <juanchozass@gmail.com>
2021-12-22 20:43:35 +0100

    spotify api gets users

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 import secrets
 from flask import Flask, render_template, request, flash
+import spoti
+import asyncio
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
 
-@app.route("/index")
+@app.route("/")
 def index():
-    flash("give me a user")
+    flash("give me a spotify user URI pls")
     return render_template("index.html")
 
-@app.route("/greet", methods=["POST", "GET"])
-def greet():
-    flash("hello " + str(request.form['user_input']) + "!")
+@app.route("/user", methods=["POST", "GET"])
+def user():
+    user_uri = str(request.form['user_input'])
+    if (user_uri != ""):
+        userstr = asyncio.run(spoti.user(user_uri))
+        flash(userstr)
+    else:
+        flash("give me a (non empty) spotify user URI pls")
+
     return render_template("index.html")
     
---
+++ spoti.py
@@ -0,0 +1,16 @@
+import spotify
+import asyncio
+import random
+
+ALBUM_URI: str = "spotify:album:1o7kL55w6IvIGz4mfWJjfu"
+USER_URI: str = "spotify:user:4lml5veu8zto4kbph5b6toyp4"
+CLIENT_ID: str = "6b437473db9b4770a42d3f29f5786137"
+CLIENT_SECRET: str = "b0ca0af3a2634a349d3b4a574b3f831f"
+
+async def user(user_uri: str) -> None:
+    # Useful tip: use a context manager to handle
+    # automatically closing any underlying http sessions
+    async with spotify.Client(CLIENT_ID, CLIENT_SECRET) as client:
+        u = await client.get_user(user_uri)
+        return f"{u.display_name} with id {u.id} from {u.country}"
+
